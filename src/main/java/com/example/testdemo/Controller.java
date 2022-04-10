c31167e453c766652b37346063e1ba506e3a61df
xzwnp <2633247964@qq.com>
2022-04-10 16:55:16 +0800

    second commit

--- src/main/java/com/example/testdemo/Controller.java
+++ src/main/java/com/example/testdemo/Controller.java
@@ -1,6 +1,7 @@
 package com.example.testdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,9 @@ public class Controller {
     @GetMapping("hello")
     public String sayHello(){
         return "hello world";
+    }
+    @GetMapping("hello/{name}")
+    public String sayHello(@PathVariable String name){
+        return "你好,"+name;
     }
 }
